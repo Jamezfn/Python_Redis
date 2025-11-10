0598700a07657a275f602f84782a2b7acaf376c8
Jamezfn <jamesmakachia19@gmail.com>
2025-11-11 00:09:57 +0300

    demo on sets

--- Redis1/redis2.py
+++ Redis1/redis2.py
@@ -1,1 +1,13 @@
+import redis
 
+client = redis.Redis(host='127.0.0.1', port=6379)
+
+### demo the sets
+
+client.sadd('pylist', "value1", "value2", "value3", "value4")
+client.sadd('plist', "value4", "value5", "value6", "value7")
+
+print(client.sinter('pylist', 'plist'))
+print(client.sunion('pylist', 'plist'))
+print(client.scard('pylist'))
+print(client.scard('plist'))
